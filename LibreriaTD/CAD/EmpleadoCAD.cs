503cf1f62bd036d224c0cb7527182c771079c83f
albertosapina <albertosapina@gmail.com>
2015-04-21 20:53:58 +0200

    CAD y EN de empleado
    CAD y EN de empleado creados

---
+++ LibreriaTD/CAD/EmpleadoCAD.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using LibreriaTD.EN;
+
+namespace LibreriaTD.CAD
+{
+    class EmpleadoCAD
+    {
+        string conexion;//Variable que se usará para guardar la cadena de conexión con la BD
+
+        public EmpleadoCAD()
+        {
+            //Aquí se adquirirá la cadena de conexión
+        }
+
+        //Método para insertar un empleado en la BD
+        //devuelve un tipo bool dependiendo de si ha podido insertar
+        public bool InsertarEmpleado(string dni, string nombre, string apellidos, string direccion, string numContacto, string email, string usuario, string pass)
+        {
+            bool insert = false;
+            return insert;
+        }
+
+        //Método para borrar un empleado de la BD
+        //devuelve un tipo bool dependiendo de si ha podido insertar
+        public bool BorrarEmpleado(string dni)
+        {
+            bool insert = false;
+            return insert;
+        }
+
+        //Método que modifica la dirección de un empleado y actualiza la BD
+        //devuelve un tipo bool dependiendo de si ha podido actualizar
+        public bool ModDireccion(string dni, string nuevaDireccion)
+        {
+            bool insert = false;
+            return insert;
+        }
+
+        //Método que modifica el email de un empleado y actualiza la BD
+        //devuelve un tipo bool dependiendo de si ha podido actualizar
+        public bool ModEmail(string dni, string nuevoEmail)
+        {
+            bool insert = false;
+            return insert;
+        }
+
+        //Método que modifica el nombre de usuario de un empleado y actualiza la BD
+        //devuelve un tipo bool dependiendo de si ha podido actualizar
+        public bool ModUsuario(string dni, string nuevoUsuario)
+        {
+            bool insert = false;
+            return insert;
+        }
+
+        //Método que modifica la contraseña de un empleado y actualiza la BD
+        //devuelve un tipo bool dependiendo de si ha podido actualizar
+        public bool ModPass(string dni, string nuevaPass)
+        {
+            bool insert = false;
+            return insert;
+        }
+
+        //Método que saca toda la información de un empleado asociada a un dni
+        //devuelve toda la información de un empleado
+        public EmpleadoEN SacarEmpleado(string dni)
+        {
+            EmpleadoEN empleado;
+            return empleado;
+        }
+
+        //Método que actualiza la información de un empleado en la BD
+        public void ActualizarEmpleado(EmpleadoEN empleado)
+        {
+
+        }
+    }
+}
---
+++ LibreriaTD/EN/EmpleadoEN.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LibreriaTD.EN
+{
+    class EmpleadoEN
+    {
+        //Atributos de la clase empleado
+        private string dni;
+        private string nombre;
+        private string apellidos;
+        private string direccion;
+        private string numContacto; //Esta variable es del tipo string por si se quiere poner el prefijo de país ej: (+34 XXXXXXXXX)
+        private string email;
+        private string usuario;
+        private string pass; //Contraseña
+
+        //Constructor
+
+        public EmpleadoEN(string dni, string nombre, string apellidos, string direccion, string numContacto, string email, string usuario, string pass)
+        {
+            this.dni = dni;
+            this.nombre = nombre;
+            this.apellidos = apellidos;
+            this.direccion = direccion;
+            this.numContacto = numContacto;
+            this.email = email;
+            this.usuario = usuario;
+            this.pass = pass;
+        }
+
+        //Getters / Setters
+        public string Dni
+        {
+            get { return dni; }
+            set { dni = value; }
+        }
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+        public string Apellidos
+        {
+            get { return apellidos; }
+            set { apellidos = value; }
+        }
+        public string Direccion
+        {
+            get { return direccion; }
+            set { direccion = value; }
+        }
+        public string NumContacto
+        {
+            get { return numContacto; }
+            set { numContacto = value; }
+        }
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+        public string Usuario
+        {
+            get { return usuario; }
+            set { usuario = value; }
+        }
+        public string Pass
+        {
+            get { return pass; }
+            set { pass = value; }
+        }
+    }
+}
