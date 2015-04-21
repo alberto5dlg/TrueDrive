87697c3b2ae2c4211c309c03ed5fa5352e5564ac
Alberto <alberto5dlg@gmail.com>
2015-04-21 18:15:26 +0200

    Terminada la EN de coche, falta implementar el CAD pero tiene lo basico para compilar,

---
+++ LibreriaTD/CAD/CocheCAD.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using LibreriaTD.EN;
+
+namespace LibreriaTD.CAD
+{
+    public class CocheCAD
+    {
+        /*Con esta funcion insertaremos un coche en nuestra base de datos
+        * devolvera verdadero cuando se inserte correctamente en caso contrario 
+        * devolvera false
+         */
+        public bool InsertarCoche(CocheEN nuevoCoche)
+        {
+            bool insert = false;
+            return insert;
+        }
+        /*Borramos un coche cuando no queden mas unidades de este y no dispongamos de el en stock
+         * la funcion de tipo bool devolvera true en caso de que se borre el coche, y si el coche, 
+         * no se ha borrado devolvera false
+         */
+        public bool BorrarCoche(CocheEN borrarCoche)
+        {
+            bool borrado = false;
+            return borrado;
+        }
+        /* Modificaremos los kilometros de un coche cuando este coche sea alquilado por 
+         * algun cliente y tenga mas de los kilometros que tenia antes de ser alquilado
+         */
+        public bool modKM(float modKm)
+        {
+            bool modifi = false;
+            return modifi;
+        }
+        /*En caso de que no se pueda borrar un coche porque todavia tiene unidades en stock
+         * restaremos una unidad que sera pasada por parametro, porque este coche todavia estara
+         * en stock
+         */
+        public bool modUds(int mUnidad)
+        {
+            bool modifi = false;
+            return modifi;
+        }
+
+        /*Modificar el precio del coche
+         */
+        public bool modPrecio(float mPre)
+        {
+            bool modifi = false;
+            return false;
+        }
+    }
+}
---
+++ LibreriaTD/EN/CocheEN.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using LibreriaTD.CAD;
+
+namespace LibreriaTD.EN
+{
+    //Entidad correspondiente a un coche
+    public class CocheEN
+    {
+        //Variables privadas que definen los atributos del coche. 
+        private string marca;
+        private string modelo;
+        private float precio;
+        private int puertas;
+        private string motor;
+        private float km;
+        private int anyo;
+        private string combustible;
+        private int plazas;
+        private string cambio;
+        private string color;
+        private int unidades;
+
+        // Getters y  Setters basicos para el funcionamiento de la entidad
+        public string Marca
+        {
+            get { return marca; }
+            set { marca = value; }
+        }
+
+        public string Modelo
+        {
+            get { return modelo; }
+            set { modelo = value; }
+        }
+
+        public float Precio
+        {
+            get { return precio; }
+            set { precio = value; }
+        }
+
+        public int Puertas
+        {
+            get { return puertas; }
+            set {puertas = value; }
+        }
+
+        public string Motor
+        {
+            get { return motor; }
+            set { motor = value; }
+        }
+
+        public float Km
+        {
+            get { return km; }
+            set { km = value; }
+        }
+
+        public int Anyo
+        {
+            get { return anyo; }
+            set { anyo = value; }
+        }
+
+        public string Combustible
+        {
+            get { return combustible; }
+            set { combustible = value; }
+        }
+
+        public int Plazas
+        {
+            get { return plazas; }
+            set { plazas = value; }
+        }
+
+        public string Cambio
+        {
+            get { return cambio; }
+            set { cambio = value; }
+        }
+
+        public string Color
+        {
+            get { return color; }
+            set { color = value; }
+        }
+
+        public int Unidades
+        {
+            get { return unidades; }
+            set { unidades = value; }
+        }
+        //Cuando adquiramos un nuevo coche en nuestro catalogo lo insertaremos en la base de datos
+        public void insertarCoche()
+        {
+            bool insert = false;
+            CocheCAD cocheCad = new CocheCAD();
+            insert = cocheCad.InsertarCoche(this);
+            if (!insert) //en caso de que no se pueda insertar el coche pq ya esta en la base de datos
+            {            //modificaremos las unidades 
+                this.Unidades = unidades + 1;
+                cocheCad.modUds(this.unidades);
+            }
+        }
+        //se borrara un coche por completo cuando no queden mas unidades en stock
+        public void borrarCoche()
+        {
+            bool borra = false;
+            CocheCAD cocheCad = new CocheCAD();
+            borra = cocheCad.BorrarCoche(this);
+            if (borra == false) // en caso de que no se pueda borrar se llamara a otra funcion 
+            {                   //para modificar las unidades
+                this.Unidades = unidades - 1; //decremento las unidades disponibles
+                cocheCad.modUds(this.unidades);
+            }
+        }
+
+        public void modKM()
+        {
+            //se modificaran cuando el coche sea alquilado y tenga mas km. 
+            CocheCAD cocheCad = new CocheCAD();
+            cocheCad.modKM(km);
+        }
+        //En caso de subida o bajada de precio del vehiculo, tendremos que modificar su precio 
+        public void modPrecio()
+        {
+            CocheCAD cocheCad = new CocheCAD();
+            cocheCad.modPrecio(precio);
+        }
+
+        
+    }
+}
