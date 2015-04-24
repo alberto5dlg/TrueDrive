0a94b6136e0d63b2b1e8f40aa7348fbca46b7d3d
Alberto de Lucas Gómez <alberto5dlg@gmail.com>
2015-04-24 17:48:37 +0200

    Añadidas los ficheros ClienteCAD y ClienteEN.
    Todo compila y ejecuta, listo para enviar al profesor

---
+++ LibreriaTD/CAD/ClienteCAD.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using LibreriaTD.EN;
+using System.Data;
+
+namespace LibreriaTD.CAD
+{
+    class ClienteCAD
+    {
+        //private string conexion;
+        
+        public ClienteCAD()
+        {
+        }
+
+        public bool InsertarCliente(ClienteEN newCliente)
+        {
+            bool insert = false;
+            return insert;
+        }
+
+        public bool BorrarCliente(ClienteEN borCliente)
+        {
+            bool delete = false;
+            return delete;
+        }
+
+        public bool modDireccion(string dni, string direccion,string ciudad)
+        {
+            bool modify = false;
+            return modify;
+        }
+
+        public bool modTelefono(string telefono)
+        {
+            bool modify = false;
+            return modify;
+        }
+    }
+}
---
+++ LibreriaTD/EN/ClienteEN.cs
@@ -0,0 +1,121 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using LibreriaTD.CAD;
+
+namespace LibreriaTD.EN
+{
+    public class ClienteEN
+    {
+        private string nif;
+        private string nombre;
+        private string apellidos;
+        private string email;
+        private string direccion;
+        private string ciudad;
+        private string pais;
+        private string telefono;
+        private string interesadoEn;
+        private string fechaNacimiento;
+
+        public ClienteEN(string nif, string nombre, string apellidos, string email, string direccion,
+            string ciudad, string pais, string telefono, string interesadoEn, string fechaNac)
+        {
+            this.nif = nif;
+            this.nombre = nombre;
+            this.apellidos = apellidos;
+            this.email = email;
+            this.direccion = direccion;
+            this.ciudad = ciudad;
+            this.pais = pais;
+            this.telefono = telefono;
+            this.interesadoEn = interesadoEn;
+            this.fechaNacimiento = fechaNac;
+        }
+
+        public string nifCliente
+        {
+            get { return nif; }
+            set { nif = value; }
+        }
+
+        public string nombreCliente
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+        public string apellidosCliente
+        {
+            get { return apellidos; }
+            set { apellidos = value; }
+        }
+
+        public string emailCliente
+        {
+            get { return email; }
+            set { email = value; }
+        }
+
+        public string direccionCliente
+        {
+            get { return direccion; }
+            set { direccion = value; }
+        }
+
+        public string ciudadCliente
+        {
+            get { return ciudad; }
+            set { ciudad = value; }
+        }
+
+        public string paisCliente
+        {
+            get { return pais; }
+            set { pais = value; }
+        }
+
+        public string telefonoCliente
+        {
+            get { return telefono; }
+            set { telefono = value; }
+        }
+
+        public string interesadoEnCliente
+        {
+            get { return interesadoEn; }
+            set { interesadoEn = value; }
+        }
+
+        public string anyoNacimientoCliente
+        {
+            get { return fechaNacimiento; }
+            set { fechaNacimiento = value; }
+        }
+
+        public void InsertarCliente()
+        {
+            ClienteCAD nuevoCliente = new ClienteCAD();
+            nuevoCliente.InsertarCliente(this);
+        }
+
+        public void BorrarCliente()
+        {
+            ClienteCAD delCliente = new ClienteCAD();
+            delCliente.BorrarCliente(this);
+        }
+
+        public void modDireccion(string newDirecc,string newCiudad)
+        {
+            ClienteCAD modDir = new ClienteCAD();
+            modDir.modDireccion(this.nif, newDirecc, newCiudad);
+        }
+
+        public void modTelefono(string newTelf)
+        {
+            ClienteCAD modTelf = new ClienteCAD();
+            modTelf.modTelefono(newTelf);
+        }
+    }
+}
