504dc644b622a6509e5c319c591ff5fc0ba97156
silexman <darkplookoon@gmail.com>
2015-05-05 16:31:37 +0200

    Script eliminado y normalizada estructura
    Todo en ingles y eliminado script que fallaba en master page.

--- CapaInterfaz/TrueDrive.Master.designer.cs
+++ CapaInterfaz/TrueDrive.Master.designer.cs
@@ -40,6 +40,15 @@ namespace CapaInterfaz {
         protected global::System.Web.UI.WebControls.TextBox searchInput;
         
         /// <summary>
+        /// Control searchSubmit.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LinkButton searchSubmit;
+        
+        /// <summary>
         /// Control userView.
         /// </summary>
         /// <remarks>
