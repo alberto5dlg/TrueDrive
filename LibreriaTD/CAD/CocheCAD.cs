8de9481613bf208becbeb872d11503c1db66aa02
Alberto de Lucas Gómez <alberto5dlg@gmail.com>
2015-05-05 17:28:58 +0200

    cambios menores

--- LibreriaTD/CAD/CocheCAD.cs
+++ LibreriaTD/CAD/CocheCAD.cs
@@ -37,7 +37,7 @@ namespace LibreriaTD.CAD
         /* Modificaremos los kilometros de un coche cuando este coche sea alquilado por 
          * algun cliente y tenga mas de los kilometros que tenia antes de ser alquilado
          */
-        public bool modKM(float modKm)
+        public bool modKM(double modKm)
         {
             bool modifi = false;
             return modifi;
@@ -54,7 +54,7 @@ namespace LibreriaTD.CAD
 
         /*Modificar el precio del coche
          */
-        public bool modPrecio(float mPre)
+        public bool modPrecio(double mPre)
         {
             bool modifi = false;
             return modifi;
